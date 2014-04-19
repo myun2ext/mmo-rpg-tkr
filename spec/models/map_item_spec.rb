cefb9fcc177e1bf819f9d932cef0f37d1043a003
myun2 <myun2@nwhite.info>
2014-04-19 10:36:28 +0000

    map_item_spec

--- spec/models/map_item_spec.rb
+++ spec/models/map_item_spec.rb
@@ -1,6 +1,18 @@
 describe MapItem do
   describe "#new" do
-    subject { MapItem.new() }
+    let(:ex_map_chip) { MapChip.new }
+
+    subject do
+      MapItem.new(
+        x: 3,
+        y: 6,
+        map_chip: ex_map_chip
+      )
+    end
+
     it { be_valid }
+    its(:x) { eq 3 }
+    its(:y) { eq 6 }
+    its(:map_chip) { eq ex_map_chip }
   end
 end
