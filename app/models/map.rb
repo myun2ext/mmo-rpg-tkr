34aca4f11ec7fa3151c529fbfb2ac8199a67a851
myun2 <myun2@nwhite.info>
2014-04-19 11:28:36 +0000

    Map: Uniqueness Validation Test

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,6 +1,15 @@
 class Map < ActiveRecord::Base
   validates :x, :y,
-    uniqueness: true,
     numericality: true,
     presence: true
+
+  validate :x, uniqueness: { scope: :x }
+
+  def self.append(args)
+    self.create(args)
+  end
+
+  class << self
+    alias :add :append
+  end
 end
--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -27,6 +27,28 @@ describe Map do
       context "Y Specified not numericaly String" do
         it { Map.new(x: 3, y: "NOT NUMERIC").should_not be_valid }
       end
+
+      describe "uniqueness" do
+        before do
+          Map.add(x: 3, y: 6)
+        end
+
+        context "Not Duplicated X/Y" do
+          it { Map.append(x: 4, y: 8).should be_valid }
+        end
+
+        context "Duplicated by X, but not duplicated X/Y" do
+          it { Map.append(x: 3, y: 8).should be_valid }
+        end
+
+        context "Duplicated by Y, but not duplicated X/Y" do
+          it { Map.append(x: 4, y: 6).should be_valid }
+        end
+
+        context "Duplicated X/Y" do
+          it { Map.append(x: 3, y: 6).should_not be_valid }
+        end
+      end
     end
   end
 end
