7ac60fe3e8dcda1b04b96611483b1cf1a0b554b4
myun2 <myun2@nwhite.info>
2014-04-19 14:43:25 +0000

    seed update

--- db/seed/development.rb
+++
@@ -1,4 +0,0 @@
-MapChip.create(name: "Hoge1")
-MapChip.create(name: "Hoge2")
-MapChip.create(name: "Grass")
-MapChip.create(name: "Soil")
---
+++ db/seeds/development.rb
@@ -0,0 +1,12 @@
+chip = MapChip.create(name: "White Tile")
+MapChip.create(name: "Black Tile")
+MapChip.create(name: "Soil")
+MapChip.create(name: "Grass")
+MapChip.create(name: "Wood")
+MapChip.create(name: "Stone Tile")
+MapChip.create(name: "Cobble Tile")
+
+Map.append(0, 0, chip)
+Map.append(1, 0, chip)
+Map.append(0, 1, chip)
+Map.append(1, 1, chip)
